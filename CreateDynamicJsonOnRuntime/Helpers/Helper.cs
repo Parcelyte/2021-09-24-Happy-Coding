a4ed5f647e1062f29a0c53d8906c42396b012b81
Alexander Schenk <Alexander.Schenk@plan-b-gmbh.com>
2021-09-24 14:39:38 +0200

    Added dynamic creation of JSON on runtime.

---
+++ CreateDynamicJsonOnRuntime/Helpers/Helper.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using XmlAndJsonExamples.Helpers;
+
+namespace CreateDynamicJsonOnRuntime.Helpers
+{
+    public class Helper
+    {
+        /// <summary>
+        /// Erstellt oder überschreibt ein (bereits existierendes) Json Dokument im angegebenen Pfad.
+        /// </summary>
+        /// <param name="jsonStr">Die Zeichenkette, welche das/die Json Objekt/e enthält.</param>
+        /// <param name="path">Der Ausgabepfad.</param>
+        public static void WriteToTextFile(string jsonStr, string path)
+        {
+            try
+            {
+                File.WriteAllText(path, jsonStr);
+                Console.WriteLine("\n" + $"JSON-Datei erfolgreich erstellt/überschrieben im Pfad [{path}]" + "\n");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Es ist ein Fehler aufgetreten. Details: {ex.Message}");
+            }
+        }
+
+        /// <summary>
+        /// Überprüft die Eingabe, welche die Anzahl der Eigenschaften bestimmt.
+        /// </summary>
+        /// <returns>Die Anzahl der Eigenschaften.</returns>
+        public static int VerifyPropertyAmountInput()
+        {
+            int propAmount;
+            string input;
+
+            Console.WriteLine("Wie viele Eigenschaften soll ihr Objekt besitzen?");
+
+            do
+            {
+                Console.Write("Geben Sie die Anzahl der Eigenschaften ein: ");
+                input = Console.ReadLine();
+                Print.ClearPreviousLines(1);
+            } while (!Int32.TryParse(input, out propAmount));
+
+            return propAmount;
+        }
+
+        /// <summary>
+        /// Überprüft die Eingabe, welche den Datentyp der aktuellen Eigenschaft besitzt.
+        /// </summary>
+        /// <returns></returns>
+        public static string VerifyPropertyTypeInput()
+        {
+            string propType;
+
+            List<string> validTypes = new List<string>
+            {
+                "bool",
+                "int",
+                "string"
+            };
+
+            do
+            {
+                Console.WriteLine("\n" + "Unterstützte Datentypen: 'string', 'int', 'bool'");
+                Console.Write($"Geben Sie den Datentyp für die Eigenschaft ein: ");
+                propType = Console.ReadLine();
+                Print.ClearPreviousLines(1);
+            } while (!validTypes.Contains(propType));
+
+            return propType;
+        }
+    }
+}
---
+++ CreateDynamicJsonOnRuntime/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using CreateDynamicJsonOnRuntime.Helpers;
+using XmlAndJsonExamples.Worker;
+
+namespace CreateDynamicJsonOnRuntime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, object> properties = new Dictionary<string, object>();
+            object objValue = new object();
+            int propAmount = 0;
+
+            propAmount = Helper.VerifyPropertyAmountInput();                  
+
+            for (int i = 0; i < propAmount; i++)
+            {
+
+                string propType = Helper.VerifyPropertyTypeInput();
+
+                Console.Write($"Geben Sie den Namen für das {i + 1}. Property ein: ");
+                string propName = Console.ReadLine();
+
+                switch (propType)
+                {
+                    case "int":
+                        Console.Write($"Geben Sie nun den Wert für das {i + 1}. Property ein: ");
+                        objValue = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case "bool":
+                        Console.Write($"Geben Sie nun den Wert für das {i + 1}. Property ein: ");
+                        objValue = Convert.ToBoolean(Console.ReadLine());
+                        break;
+                    case "string":
+                        Console.Write($"Geben Sie nun den Wert für das {i + 1}. Property ein: ");
+                        objValue = Console.ReadLine();
+                        break;
+                    default:
+                        Environment.Exit(0);
+                        break;
+                }
+
+                properties.Add(propName, objValue);
+            }
+
+            Console.Write("\n" + "Geben Sie den Pfad für das JSON-Dokument ein: ");
+
+            string path = Console.ReadLine();
+
+            path += path.EndsWith(".json") ? "" : ".json";
+
+            string jsonStr = JsonWorker.CreateJsonStringFromObject(properties, true);
+
+            Console.WriteLine("\n" + jsonStr);
+
+            JsonWorker.WriteToFile(jsonStr, path);
+        }
+    }
+}
